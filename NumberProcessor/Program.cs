5048860653497d7e0adad437f00f6de100376a9e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-17 09:54:35 +0300

    Initial commit

---
+++ NumberProcessor/Program.cs
@@ -0,0 +1,34 @@
+﻿class NumberProcessor
+{
+    public static void Main(string[]args)
+    {
+        
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        string command;
+        do
+        {
+            command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "Inc":
+                    number++;
+                    break;
+                case "Dec":
+                    number--;
+                    break;
+                case "End":
+                    Console.WriteLine(number);
+                    break;
+                
+            }
+        } while (command != "End");
+    }
+}
+  
+
+                        
+
+        
+    
